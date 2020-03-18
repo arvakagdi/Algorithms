e166cfeced4d0e0614bb7c20a42440a9b44a4b26
Arva Kagdi <60667228+arvakagdi@users.noreply.github.com>
2020-03-18 15:30:06 -0400

    Fibonacci Numbers Using List | Iterative | Python

---
+++ fibIterativeUsingList.py
@@ -0,0 +1,22 @@
+'''
+Fibonacci Numbers using List | Iterative solution
+'''
+
+def fibonacci_number(n):
+    fiblist = []
+    fiblist.append(0)
+    fiblist.append(1)
+
+    if n <= 1:
+        #   fiblist.append(n)
+        return n
+    else:
+        for i in range(2, n + 1):
+            result = fiblist[i - 1] + fiblist[i - 2]
+            fiblist.append(result)
+    return fiblist[n]
+
+
+
+print(fibonacci_number(3000))
+
